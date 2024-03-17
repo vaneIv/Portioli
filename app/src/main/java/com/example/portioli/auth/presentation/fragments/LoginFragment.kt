794fbd52ec3e5c3d1cb27c67a4538515d5fa3b4f
vaneIv <appvane@gmail.com>
2024-03-17 17:06:38 +0100

    Created layouts for CreateAccountFragment and LoginOptionsFragment.

--- app/src/main/java/com/example/portioli/auth/presentation/fragments/LoginFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.portioli.auth.presentation.fragments
-
-import androidx.fragment.app.Fragment
-import com.example.portioli.R
-
-class LoginFragment : Fragment(R.layout.fragment_login) {
-}
